e0cef7cc57b1c36701288f7b251b1b663f8b6f43
Promit-revar <promit.revar2211@gmail.com>
2023-01-15 13:12:09 +0530

    Feat: test with the designed test cases

--- isPrimeOrArmstrong/Amstrong_or_prime.js
+++ isPrimeOrArmstrong/Amstrong_or_prime.js
@@ -1,3 +1,4 @@
+const fs=require('fs');
 const isPrime=(n)=>{
     if(n===0 || n===1){
         return false;
@@ -5,9 +6,10 @@ const isPrime=(n)=>{
     let limit=Math.round(Math.sqrt(n));
     for(var i=2;i<limit;i++){
         if(n%i==0){
-            return true;
+            return false;
         }
     }
+    return true;
 }
 const isArmstrong=(n)=>{
    let  m=0,num=n;
@@ -21,7 +23,18 @@ const isArmstrong=(n)=>{
 const Main=()=>{
     // let num=prompt("Enter the Number:");
     let num=153;
-    console.log("is Prime: "+isPrime(num)+"\nis ArmStrong: "+isArmstrong(num));
+    fs.readFile('testcases.txt', (err, data) => {
+        if (err) throw err;
+        let inputs=data.toString().split('\n').map((item)=>{
+            return parseInt(item);
+        });
+        inputs.forEach(input => {
+            console.log("Number:"+input+"\nis Prime: "+isPrime(input)+"\nis ArmStrong: "+isArmstrong(input)+"\n\n\n");
+        })
+        
+        
+    })
+    //
 
 }
 Main();
