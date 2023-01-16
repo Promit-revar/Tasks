043fe5ca559abf75ff8c6e3fdf68ee584ed7b8e3
Promit-revar <promit.revar2211@gmail.com>
2023-01-16 12:11:52 +0530

    Feat: Add gitignore and change return values for StringCheck and LeapYear Problems

--- Question5/LeapYear.js
+++ Question5/LeapYear.js
@@ -1,11 +1,11 @@
 const fs=require('fs');
 exports.isLeapYear=function(year){
     if(typeof(year)!="number" || year>9999 || year<1000) return "Invalid Input";
-    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) return true;
-     return false;
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) return "true";
+     return "false";
 }
 exports.isLeapYearArrow=(year)=>{
     if(typeof(year)!="number" || year>9999 || year<1000) return "Invalid Input";
-    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) return true;
-     return false;
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) return "true";
+     return "false";
 }
--- Question6/StringCheck.js
+++ Question6/StringCheck.js
@@ -1,9 +1,9 @@
 const fs=require('fs');
 exports.StringCheckArrow=(str)=>{
     if(typeof(str) != 'string' || str.length<6) return "Invalid Input";
-    return str.substr(-6,6)==="Script";
+    return (str.substr(-6,6)==="Script").toString();
 }
 exports.StringCheck=function(str){
     if(typeof(str) != 'string' || str.length<6) return "Invalid Input";
-    return str.substr(-6,6)==="Script";
+    return (str.substr(-6,6)==="Script").toString();
 }
