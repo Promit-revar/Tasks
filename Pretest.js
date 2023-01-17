e44321cdfdbe1f11a0db0be9b273de54e3060553
Promit-revar <promit.revar2211@gmail.com>
2023-01-17 13:00:19 +0530

    feat: change name of the test.js file to pretest.js

--- Pretest.js
+++ Pretest.js
@@ -2,4 +2,3 @@ const {DoubleNumbers} = require('./NumberChanger/TwiceTheNumbers');
 const {TrippleNumbersGetEvenFilter,TrippleNumbersGetEvenReduce} = require('./NumberTripple/NumberTrippler');
 console.log(TrippleNumbersGetEvenFilter([1,2,3]));
 console.log(TrippleNumbersGetEvenReduce([1,2,3,4]));
-//console.log(getEvenFilter([1,2,3,4,5,6,7,78,8,10]));
